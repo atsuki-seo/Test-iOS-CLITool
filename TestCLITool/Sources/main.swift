751bd0483d33d85e3e70d085473fe322a08dee3f
atsuki.seo <atsuki.seo@mixi.co.jp>
2024-04-29 22:45:32 +0900

    swift-format した

--- TestCLITool/Sources/main.swift
+++ TestCLITool/Sources/main.swift
@@ -1,22 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import Foundation
 import ArgumentParser
+import Foundation
 import SwiftSyntax
 import SwiftSyntaxBuilder
 
 @main
 struct TestCLITool: ParsableCommand {
-    mutating func run() throws {
-        let source = SourceFileSyntax {
-            DeclSyntax("import Foundation")
-            ClassDeclSyntax(name: "SampleModel") {
-                DeclSyntax(#"private let title: String = "Hello""#)
-            }
+  mutating func run() throws {
+    let source = SourceFileSyntax {
+      DeclSyntax("import Foundation")
+      for i in 0...100 {
+        ClassDeclSyntax(name: "SampleModel") {
+          DeclSyntax(#"private let title: String = "Hello""#)
         }
-
-        let string = source.formatted().description
-        print(string)
+      }
     }
+
+    let string = source.formatted().description
+    print(string)
+  }
 }
